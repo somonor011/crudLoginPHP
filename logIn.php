ac006104fde7c7954f1c24ae920cf728a1a092e6
somonor011 <somonorhong011@gmail.com>
2024-11-19 16:46:15 +0700

    first commit

---
+++ logIn.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/signUp.css">
+    <title>Log In</title>
+    <style>
+        .form {
+            width: 230px;
+            height: 280px;
+        }
+    </style>
+</head>
+<body>
+    <div class="form">
+        <div class="title">
+            <p>Log In</p>
+        </div>
+        <form action="" method="post" >
+            <input type="text" name="firstName" id="" placeholder="Frist name">
+            <input type="text" name="password" id="" placeholder="Password">
+            <input type="submit" value="Log In" name="btnLogin">
+        </form>
+    </div>
+</body>
+</html>
---
+++ signUp.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/signUp.css">
+    <title>Sing Up</title>
+</head>
+<body>
+    <div class="form">
+        <div class="title">
+            <p>Sign Up</p>
+        </div>
+        <form action="" method="post" >
+            <input type="text" name="firstName" id="" placeholder="Frist name">
+            <input type="text" name="lastName" id="" placeholder="Last name">
+            <input type="text" name="email" id="" placeholder="Email">
+            <input type="text" name="password" id="" placeholder="Password">
+            <input type="text" name="confirmPassword" id="" placeholder="Confirm password">
+            <input type="submit" value="Sign Up" name="btnSignUp">
+        </form>
+    </div>
+</body>
+</html>
